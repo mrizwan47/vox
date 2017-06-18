ba4d8c10fb872a78dd3fefee4ee801cc38465f7b
Rizwan <m.rizwan_47@yahoo.com>
2017-06-18 08:14:14 +0500

    fixed cli

--- vox/__main__.py
+++ vox/__main__.py
@@ -1,16 +1,2 @@
-import click, os, time
-import vox.api as vox
-
-@click.command()
-@click.argument( 'text' )
-@click.argument( 'language', required=False )
-
-def main( text, language ):
-
-	if language is None:
-		language	=	'en'
-
-	vox.say( text, language )
-
-if __name__ == "__main__":
-	main()
+import cli
+cli.main()
